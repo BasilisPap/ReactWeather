2d7f372b6b94615ba0100f81859eea02c8406e6d
Basilis Papadopoulos <basilis.papadopoulos@gmail.com>
2016-05-12 15:50:37 -0500

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,3 +1,3 @@
 var React = require('react');
 
-module.exports = () => <div><h3>Examples Component</h3><p>Welcome to examples page</p></div>;
+module.exports = () => <div><h3>Examples</h3><p>Welcome to examples page</p></div>;
